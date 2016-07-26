6cb61b3b4321b0ad19170c86f05b2762f5ee907e
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-07-26 15:53:31 +0300

    create pokemon entity

--- pokemon.rb
+++ pokemon.rb
@@ -1,2 +1,29 @@
 class Pokemon
+
+    attr_reader :name, :cp
+
+    def initialize(name, cp=0)
+      @name=name.capitalize
+      @cp=cp
+    end
+
+    def power_up(pow)
+      @cp+=pow.to_i
+    end
+
+    def power_down(pow)
+      @cp-=pow.to_i
+    end
+
+    def info
+      "#{@name}-#{@cp}"
+    end
+
 end
+
+p=Pokemon.new 'Bulbazaur', 20
+puts p.name
+puts p.cp
+puts p.power_up 100
+puts p.power_down 10
+puts p.info
