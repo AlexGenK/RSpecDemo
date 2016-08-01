f4474a5b47683c8a7ebbb2a2028f4f8a7e1fe043
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-08-01 22:19:31 +0300

    add data and methods in Plane class

--- plane.rb
+++ plane.rb
@@ -1,3 +1,24 @@
 # класс - самолет
-class Plane
+class Boeing737
+
+  KILOGRAMM_PER_HOUR=2600
+
+  attr_reader :fuel
+
+  def initialize
+    @fuel=0
+  end
+
+  def add_fuel amount
+    @fuel+=amount
+  end
+
+  def flight_duration
+    0
+  end
+
 end
+
+plane=Boeing737.new
+plane.add_fuel 20.000
+puts "Flight duration is #{plane.flight_duration} hours"
